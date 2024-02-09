f79cb6e7d2c22e43bc75f509d37430b789bd7d5d
mehul <mehulmanhas26@gmail.com>
2024-02-10 01:14:27 +0530

    Adding a new JPA method in the interface

--- src/main/java/com/ecommerceproject/ECommProject/Repository/ProductRepository.java
+++ src/main/java/com/ecommerceproject/ECommProject/Repository/ProductRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Product findByTitle(String title);
 }
