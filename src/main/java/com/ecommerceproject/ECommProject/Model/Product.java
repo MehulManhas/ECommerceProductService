764714052f31655331a3e6802c28cfc6ba3ed765
mehul <mehulmanhas26@gmail.com>
2024-02-10 01:12:58 +0530

    Adding and updating classes

--- src/main/java/com/ecommerceproject/ECommProject/Model/Product.java
+++ src/main/java/com/ecommerceproject/ECommProject/Model/Product.java
@@ -1,19 +1,25 @@
 package com.ecommerceproject.ECommProject.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Product extends BaseModel{
     private String title;
-    private double price;
+
     private  String description;
     private String image;
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
+    @OneToOne
+    private Price price;
+    @ManyToMany(mappedBy = "product")
+    private List<ProductOrder> productOrders;
 }
---
+++ src/main/java/com/ecommerceproject/ECommProject/Model/ProductOrder.java
@@ -0,0 +1,16 @@
+package com.ecommerceproject.ECommProject.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+@Entity
+public class ProductOrder extends BaseModel{
+    private double price;
+    @ManyToMany
+    private List<Product> product;
+}
