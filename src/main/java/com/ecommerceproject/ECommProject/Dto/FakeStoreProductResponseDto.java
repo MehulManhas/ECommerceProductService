3a68cbb36c05183630873fe3625ded4e43257ecd
mehul <mehulmanhas26@gmail.com>
2024-02-10 01:11:57 +0530

    Changing id dtype to UUID

--- src/main/java/com/ecommerceproject/ECommProject/Dto/FakeStoreProductResponseDto.java
+++ src/main/java/com/ecommerceproject/ECommProject/Dto/FakeStoreProductResponseDto.java
@@ -3,10 +3,12 @@ package com.ecommerceproject.ECommProject.Dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 public class FakeStoreProductResponseDto {
-    private int id;
+    private UUID id;
     private String title;
     private double price;
     private String category;
--- src/main/java/com/ecommerceproject/ECommProject/Dto/ProductResponseDto.java
+++ src/main/java/com/ecommerceproject/ECommProject/Dto/ProductResponseDto.java
@@ -1,12 +1,15 @@
 package com.ecommerceproject.ECommProject.Dto;
 
+import com.ecommerceproject.ECommProject.Model.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Getter
 @Setter
 public class ProductResponseDto {
-    private int id;
+    private UUID id;
     private String title;
     private double price;
     private String category;
