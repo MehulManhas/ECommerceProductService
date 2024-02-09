148e8bd49e920410f87c9f9d728a8a859369686b
mehul <mehulmanhas26@gmail.com>
2024-02-10 01:13:22 +0530

    Adding two new mapping methods

--- src/main/java/com/ecommerceproject/ECommProject/Mapper/ProductMapper.java
+++ src/main/java/com/ecommerceproject/ECommProject/Mapper/ProductMapper.java
@@ -1,9 +1,9 @@
 package com.ecommerceproject.ECommProject.Mapper;
 
-import com.ecommerceproject.ECommProject.Dto.FakeStoreProductRequestDto;
-import com.ecommerceproject.ECommProject.Dto.FakeStoreProductResponseDto;
-import com.ecommerceproject.ECommProject.Dto.ProductRequestDto;
-import com.ecommerceproject.ECommProject.Dto.ProductResponseDto;
+import com.ecommerceproject.ECommProject.Dto.*;
+import com.ecommerceproject.ECommProject.Model.Product;
+
+import java.util.List;
 
 public class ProductMapper {
     public static FakeStoreProductRequestDto productRequestToFakeStoreProductRequest(ProductRequestDto productRequestDto){
@@ -25,6 +25,37 @@ public class ProductMapper {
         productResponseDto.setDescription(fakeStoreProductResponseDto.getDescription());
         productResponseDto.setImage(fakeStoreProductResponseDto.getImage());
         productResponseDto.setPrice(fakeStoreProductResponseDto.getPrice());
+        return productResponseDto;
+    }
+
+    public static ProductListResponseDto productListToProductResponseDto(List<Product> productList){
+
+        ProductListResponseDto productListResponseDto = new ProductListResponseDto();
+
+        for(Product p : productList){
+            ProductResponseDto productResponseDto = new ProductResponseDto();
+            productResponseDto.setPrice(p.getPrice().getAmount());
+            productResponseDto.setId(p.getId());
+            productResponseDto.setTitle(p.getTitle());
+            productResponseDto.setDescription(p.getDescription());
+            productResponseDto.setImage(p.getImage());
+            productResponseDto.setCategory(p.getCategory().getCategoryName());
+
+            productListResponseDto.getProductList().add(productResponseDto);
+        }
+
+        return productListResponseDto;
+    }
+
+    public static ProductResponseDto productToProductResponseDto(Product product){
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setPrice(product.getPrice().getAmount());
+        productResponseDto.setId(product.getId());
+        productResponseDto.setTitle(product.getTitle());
+        productResponseDto.setDescription(product.getDescription());
+        productResponseDto.setImage(product.getImage());
+        productResponseDto.setCategory(product.getCategory().getCategoryName());
+
         return productResponseDto;
     }
 }
